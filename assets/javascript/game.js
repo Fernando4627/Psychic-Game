6d3bf10e7eabb1a5cb5e30bf2ee2c0acc13f6021
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-08-20 06:04:30 -0700

    Psuedo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,41 @@
 //varaiables
-var computerChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var playerGuess
+var computerChoice = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var playerGuess="";
 var guessLeft = 9;
-var wins;
-var losses;
+var wins=0;
+var losses=0;
 var computerGuess;
-document.onkeyup = function (event) {
-    playerGuess = event.key;
-    playerGuess.toUpperCase()
-document.getElementById("Guess").innerHTML=playerGuess
-    console.log(playerGuess)
+//computer Guess
+//Computer's guess .... needs to stop and wait for player to hit a loss before changing
 computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 console.log(computerGuess)
-}
+
+
+//key up function
+document.onkeyup = function (event) {
+    playerGuess = event.key;
+//Writes on html players guess
+    document.getElementById("Guess").innerHTML=playerGuess
+    console.log(playerGuess)
+};
+
+//rules 
+//if player Guess right
 if (playerGuess === computerGuess) {
     wins++;
-    //document.getElementsById("win").innerHTML=wins;
-    //console.log(wins)
-
-} else if (playerGuess !== computerGuess) {
+    document.getElementById("win").innerHTML=wins;
+    console.log(wins);
+//player's lives and reduces when player does not guess right
+} else(playerGuess!== computerGuess){
     guessLeft--;
-    document.getElementsById("countdown").innerHTML=losses;
-    console.log(guessLeft)
+    document.getElementsById("countdown").innerHTML=guessLeft;
+    console.log(guessLeft);
 }
-if (guessLeft === 0) {
-    losses++
+//when player is out of guesses they lose and new letter is chosen
+if (guessLeft = 0) {
+    losses++;
     document.getElementsById("loss").innerHTML=losses;
-    console.log(losses)
+    console.log(losses);
 };
 
