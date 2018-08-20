4281128d613344b8e0bef21e5a05bf8a5b37509a
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-08-20 05:28:21 -0700

    Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,31 @@
 //varaiables
 var computerChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var playerGuess 
+var playerGuess
 var guessLeft = 9;
-var wins= 0;
-var losses = 0;
+var wins;
+var losses;
+var computerGuess;
 document.onkeyup = function (event) {
-playerGuess=event.key;
-var computerGuess=computerChoice[Math.floor(Math.random()* computerChoice.length)];}
-if (playerGuess===computerGuess) {
-wins++    
-}else if (playerGuess!==computerGuess) {
+    playerGuess = event.key;
+    playerGuess.toUpperCase()
+document.getElementById("Guess").innerHTML=playerGuess
+    console.log(playerGuess)
+computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+console.log(computerGuess)
+}
+if (playerGuess === computerGuess) {
+    wins++;
+    //document.getElementsById("win").innerHTML=wins;
+    //console.log(wins)
+
+} else if (playerGuess !== computerGuess) {
     guessLeft--;
+    document.getElementsById("countdown").innerHTML=losses;
+    console.log(guessLeft)
 }
-if (guessLeft===0) {
+if (guessLeft === 0) {
     losses++
-}
+    document.getElementsById("loss").innerHTML=losses;
+    console.log(losses)
+};
+
